8f00b16fe572a446e514fdca46d39e7bd96dc5f7
Azamat Gaparov <agaparovkg@gmail.com>
2023-05-19 20:01:29 -0700

    latest changes

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import UsersTable from "./UsersTable";
 import axios from "axios";
 import Search from "./Search";
+import Loader from "./Loader";
 
 const url = "https://61008c3dbca46600171cf917.mockapi.io/api/v1/users";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [show, setShow] = useState(false);
+
+  const toggleModal = (id) => {
+    setShow(!show);
+    console.log(id);
+  };
 
   const fetchUsers = async () => {
+    setLoading(false);
+
     try {
       //   const res = await fetch(url);
       //   const d = await res.json();
@@ -21,8 +32,11 @@ const Users = () => {
       setUsers(data);
     } catch (err) {
       console.log(err);
+      setErrorMessage(err.message);
     } finally {
-      console.log("finally block");
+      setTimeout(() => {
+        setLoading(true);
+      }, 1500);
     }
   };
 
@@ -41,16 +55,68 @@ const Users = () => {
     return user.firstname.toLowerCase().includes(searchQuery.toLowerCase());
   });
 
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`${url}/${id}`);
+
+      // fetchUsers();
+      const filteredUsers = users.filter((user) => user.id !== id);
+      setUsers(filteredUsers);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      console.log("finally block");
+    }
+  };
+
+  const updateUser = async (id, updatedUser) => {
+    try {
+      const res = await axios.put(`${url}/${id}`, updatedUser);
+
+      console.log(res.data);
+      //   setUsers();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      console.log("finally block");
+    }
+  };
+
   return (
     <>
       <Container>
-        <Row className="mb-3 mt-3">
-          <Search handleSearch={handleSearch} />
-          <Col className="text-end">
-            <Button>Create a User</Button>
-          </Col>
-        </Row>
-        <UsersTable users={filteredUsers} />
+        {!loading ? (
+          <Loader />
+        ) : (
+          <>
+            <Row className="mb-3 mt-3">
+              <Search handleSearch={handleSearch} />
+              <Col className="text-end">
+                <Button>Create a User</Button>
+              </Col>
+            </Row>
+            <UsersTable
+              errorMessage={errorMessage}
+              users={filteredUsers}
+              deleteUser={deleteUser}
+              toggleModal={toggleModal}
+            />
+          </>
+        )}
+        <Modal show={show} onHide={toggleModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={toggleModal}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={toggleModal}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     </>
   );
--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Table, Button } from "react-bootstrap";
 
-const UsersTable = ({ users }) => {
+const UsersTable = ({ errorMessage, toggleModal, users, deleteUser }) => {
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
@@ -21,27 +21,35 @@ const UsersTable = ({ users }) => {
         </tr>
       </thead>
       <tbody>
-        {users.map((user, index) => {
-          const { firstname, lastname, email, birthdate, avatar, id } = user;
-          return (
-            <tr key={id}>
-              <td>{index + 1}</td>
-              <td>
-                <img src={avatar} alt={firstname} />
-              </td>
-              <td>{firstname}</td>
-              <td>{lastname}</td>
-              <td>
-                <a href="mailto:`{email}`">{email}</a>
-              </td>
-              <td>{formatDate(birthdate)}</td>
-              <td>
-                <Button>Update</Button>
-                <Button variant="danger">Delete</Button>
-              </td>
-            </tr>
-          );
-        })}
+        {users.length !== 0 ? (
+          users.map((user, index) => {
+            const { firstname, lastname, email, birthdate, avatar, id } = user;
+            return (
+              <tr key={id}>
+                <td>{index + 1}</td>
+                <td>
+                  <img src={avatar} alt={firstname} />
+                </td>
+                <td>{firstname}</td>
+                <td>{lastname}</td>
+                <td>
+                  <a href="mailto:`{email}`">{email}</a>
+                </td>
+                <td>{formatDate(birthdate)}</td>
+                <td>
+                  <Button onClick={() => toggleModal(id)}>Update</Button>
+                  <Button variant="danger" onClick={() => deleteUser(id)}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td colSpan={7}>{errorMessage}</td>
+          </tr>
+        )}
       </tbody>
     </Table>
   );
