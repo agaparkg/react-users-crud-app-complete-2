38d1984623bfd368ff3da70e2b3edcfa4b00d0cd
Azamat Gaparov <agaparovkg@gmail.com>
2023-05-19 17:57:03 -0700

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
       {/* Header */}
       <Header />
+
       {/* Users */}
+      <Users />
 
       {/* Footer */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <footer className="py-3 my-4">
+        <p className="text-center text-muted">
+          Seytech © 2023 - All Rights Reserved.
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
